9f4d36ec204671dbeb7d44d3c3d78ac80d8e5df9
ulik <arifdegozaru24@gmail.com>
2025-05-13 10:27:59 +0700

    add context configuration

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -2,17 +2,29 @@ import OpenAI from "openai";
 
 const openAI = new OpenAI();
 
-process.stdin.addListener("data", async function (input) {
-  const userInput = input.toString().trim();
+const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
+  role: "system",
+  content: "you are a helpful chatbot",
+}]
+
+async function createChatCompletion() {
   const response = await openAI.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{
-      role: "system",
-      content: "you are a helpful chatbot",
-    }, {
-      role: "user",
-      content: userInput,
-    }]
+    messages: context
   });
-  console.log(response.choices[0].message.content);
+  const responseMessage = response.choices[0].message;
+  context.push({
+    role: "assistant",
+    content: responseMessage.content
+  })
+  console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`);
+}
+
+process.stdin.addListener("data", async function (input) {
+  const userInput = input.toString().trim()
+  context.push({
+    role: "user",
+    content: userInput,
+  })
+  await createChatCompletion();
 })
