f8ef28921621232c51a0ba26298355d7fc35c808
ulik <arifdegozaru24@gmail.com>
2025-05-13 10:46:38 +0700

    limit context length by token

--- chat_app/src/basic_chat/index.ts
+++ chat_app/src/basic_chat/index.ts
@@ -1,6 +1,10 @@
 import OpenAI from "openai";
+import { encoding_for_model } from "tiktoken";
 
 const openAI = new OpenAI();
+const encoder = encoding_for_model("gpt-3.5-turbo")
+
+const MAX_TOKENS = 200;
 
 const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
   role: "system",
@@ -8,6 +12,7 @@ const context: OpenAI.Chat.Completions.ChatCompletionMessageParam[] = [{
 }]
 
 async function createChatCompletion() {
+  console.log("Context: ", context);
   const response = await openAI.chat.completions.create({
     model: "gpt-3.5-turbo",
     messages: context
@@ -17,6 +22,11 @@ async function createChatCompletion() {
     role: "assistant",
     content: responseMessage.content
   })
+
+  if (response.usage && response.usage.total_tokens > MAX_TOKENS) {
+    deleteOlderMessages();
+  }
+
   console.log(`${response.choices[0].message.role}: ${response.choices[0].message.content}`);
 }
 
@@ -28,3 +38,34 @@ process.stdin.addListener("data", async function (input) {
   })
   await createChatCompletion();
 })
+
+function deleteOlderMessages() {
+  let contextLength = getContextLength();
+  while (contextLength > MAX_TOKENS) {
+    for(let i = 0; i < contextLength; i++) {
+      const message = context[i];
+      if (message.role !== "system") {
+        context.splice(i, 1);
+        contextLength = getContextLength();
+        console.log(`New context length: ${contextLength}`);
+        break;
+      }
+    }
+  }
+}
+
+function getContextLength() {
+  let length = 0;
+  context.forEach((message) => {
+    if (typeof message.content === "string") {
+      length += encoder.encode(message.content).length
+    } else if (Array.isArray(message.content)) {
+      message.content.forEach((messageContent) => {
+        if (messageContent.type === "text") {
+          length += encoder.encode(messageContent.text).length;
+        }
+      })
+    }
+  })
+  return length;
+}
